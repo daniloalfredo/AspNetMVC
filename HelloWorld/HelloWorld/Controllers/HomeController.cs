2d3c405fe7776051c1282d1345f9ae4c433febc7
Danilo Alfredo Marinho de Souza <dams@cin.ufpe.br>
2016-10-25 14:51:10 -0300

    First commit

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Don't dream it. Be it.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
